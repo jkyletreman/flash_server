ddbf6850b45c59abbb940db7a4efb9bf1ce1a411
Kyle Treman <jkt290@nyu.edu>
2018-02-22 13:27:25 -0500

    Add schema

--- migrations/20180222131819_flash_cards.js
+++ migrations/20180222131819_flash_cards.js
@@ -1,8 +1,13 @@
 
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.createTable('flashcards', (table) => {
+    table.increments();
+    table.string('collection', 100).defaultTo('main');
+    table.string('question', 150).notNull();
+    table.text('answer').notNull();
+  })
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema.dropTable('flashcards');
 };
