46dbf9515783d0bc05b6db634fc3c2d2fb87d6f0
Kyle Treman <jkt290@nyu.edu>
2018-02-26 13:58:57 -0500

    Add test data

--- migrations/20180222131819_flash_cards.js
+++ migrations/20180222131819_flash_cards.js
@@ -2,7 +2,6 @@
 exports.up = function(knex, Promise) {
   return knex.schema.createTable('flashcards', (table) => {
     table.increments();
-    table.string('collection', 100).defaultTo('main');
     table.string('question', 150).notNull();
     table.text('answer').notNull();
   })
