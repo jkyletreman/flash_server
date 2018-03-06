46bc07a6ca97227d634373ad124636a82248a92a
Kyle Treman <jkt290@nyu.edu>
2018-03-06 10:16:10 -0500

    Add

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
-const app = express();
-const PORT = 8000;
 const morgan = require('morgan');
 const bodyParser = require('body-parser')
+const app = express();
+const PORT = 8000;
 
 // middleware
 app.use(morgan('combined'));
