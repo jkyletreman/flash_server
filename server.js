7d3b7cb8ff27291c1f497f7f65557261a8db239b
Kyle Treman <jkt290@nyu.edu>
2018-03-06 10:31:09 -0500

    Add GET all, POST

--- server.js
+++ server.js
@@ -7,15 +7,32 @@ const PORT = 8000;
 const {
   findCard,
   createCard
-} = require('../models/db_functions')
+} = require('./models/db_functions')
 // middleware
 app.use(morgan('combined'));
 app.use(bodyParser.urlencoded({ extended: true}));
 app.use(bodyParser.json());
 
 // get ALL
+app.get("/cards", (req, res) => {
+  findCard(req).then(cards => {
+    res.format({
+      "application/json":() => res.json(cards)
+    });
+  });
+});
 
 // create
+app.post("/new", (req, res) => {
+  createCard(req).then(cards => {
+  const card = cards[0];
+
+  res.format({
+    'application/json': () => res.json(card)
+  });
+});
+
+
 
 app.use('/', (req, res) => {
   res.sendStatus(404);
